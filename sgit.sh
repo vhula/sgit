c1695e2a3247cc387744ca0aac25c0bcbfb08460
Vadym <vadym.hula@gmail.com>
2017-11-24 09:46:04 +0000

    Improve sgit.sh script
    Added ability to configure username, email, core.editor
    User can specify if the configuration has to be global for git

--- sgit.sh
+++ sgit.sh
@@ -7,8 +7,22 @@ function help() {
     echo "Arguments:"
     echo -e "\t-u, --username\t\t\tname of the user for git env"
     echo -e "\t-e, --email\t\t\temail of the user for git env"
+    echo -e "\t--editor\t\t\tgit editor"
     echo "Options"
     echo -e "\t-h, --help\t\t\tprint usage"
+    echo -e "\t-c, --configure\t\t\tconfigure default aliases"
+    echo -e "\t--global\t\t\tglobal configuration"
+}
+
+CONFIG_COMMAND="git config"
+GLOBAL_CONFIG=""
+
+function configure_if_not_empty() {
+    CONFIG_NAME="${1}"
+    CONFIG_VALUE="${2}"
+    if [ ! -z "${CONFIG_VALUE}" ]; then
+      eval "${CONFIG_COMMAND} ${GLOBAL_CONFIG} ${CONFIG_NAME} \"${CONFIG_VALUE}\""
+    fi
 }
 
 while [[ $# -gt 0 ]]
@@ -21,8 +35,38 @@ case $key in
     shift
     shift
     ;;
+    -u|--username)
+    CONFIG_USERNAME="$2"
+    shift
+    shift
+    ;;
+    -e|--email)
+    CONFIG_EMAIL="$2"
+    shift
+    shift
+    ;;
+    --editor)
+    CORE_EDITOR="$2"
+    shift
+    shift
+    ;;
+    -c|--configure)
+    CONFIGURE_ALIASES="yes"
+    shift
+    shift
+    ;;
+    --global)
+    GLOBAL_CONFIG="--global"
+    shift
+    shift
+    ;;
 esac
 done
 
+configure_if_not_empty "user.name" "${CONFIG_USERNAME}"
+configure_if_not_empty "user.email" "${CONFIG_EMAIL}"
+configure_if_not_empty "core.editor" "${CORE_EDITOR}"
 
-git config --global alias.s 'status'
+if [ "${CONFIGURE_ALIASES}" = "yes" ]; then
+  echo "Aliases configuration"
+fi
